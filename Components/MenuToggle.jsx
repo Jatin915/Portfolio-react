10cf05066c31fa93d1b1213cdc231c62342a22c4
gJatin <gjatin278@gmail.com>
2025-08-07 20:10:28 +0530

    fixed a bug

--- Components/MenuToggle.jsx
+++ Components/MenuToggle.jsx
@@ -7,13 +7,13 @@ const MenuToggle = ({menuOpen, setMenuOpen}) => {
   return (
   <>
 
-    <div id="menu-container" className={`min-h-full w-full fixed top-0 z-50 p-5 flex flex-col justify-between transition-all duration-300 ${(menuOpen) ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
+    <div id="menu-container" className={`min-h-full w-full fixed top-0 z-50 p-5 flex flex-col transition-all duration-300 ${(menuOpen) ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
       <div className="flex justify-between items-center">
         <Link onClick={() => setMenuOpen(false)} to="/hero"><strong><h2 className="text-2xl dark:text-white transition duration-300">Jatin.dev</h2></strong></Link>
         <button onClick={() => setMenuOpen(false)} className="text-black dark:text-white transition duration-300 h-fit "><h1 className="text-2xl">X</h1></button>
       </div>
 
-      <div id="links" onClick={() => setMenuOpen(false)} className="w-full flex justify-center items-center">
+      <div id="links" onClick={() => setMenuOpen(false)} className="w-full flex grow justify-center items-center">
         <ul className=" flex flex-col gap-10 items-center text-xl">
           <li className="transition-all duration-300 ease-in-out hover:scale-110"><Link to="/showcase" className="cursor-pointer font-semibold dark:text-gray-50">Showcase</Link></li>
           <li className="transition-all duration-300 ease-in-out hover:scale-110"><Link to="/about" className="cursor-pointer font-semibold dark:text-gray-50">About</Link></li>
